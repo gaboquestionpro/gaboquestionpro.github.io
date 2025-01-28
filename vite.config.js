1e4cb43d6af72a80440d42fcf5a6231f70d03d01
gaborm <gabriel.rodriguez@questionpro.com>
2025-01-27 18:12:01 -0600

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
- base: "/",
+ base: "/gaboquestionpro.github.io/",
  plugins: [react()],
- preview: {
-  port: 3000,
-  strictPort: true,
- },
- server: {
-  port: 3000,
-  strictPort: true,
-  host: true,
-  origin: "http://0.0.0.0:3000",
- },
+
 });
